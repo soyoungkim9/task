b4080ccc17484b7777d1191171a0818de7784fdc
soyoungkim9 <soyoung930623@gmail.com>
2020-02-18 10:11:40 +0900

    contentType 변경 json형식으로 응답 받기 response.setContentType("application/json; charset=UTF-8");

--- file/src/main/java/base/UploadServlet.java
+++ file/src/main/java/base/UploadServlet.java
@@ -9,7 +9,7 @@ public class UploadServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     request.setCharacterEncoding("UTF-8");
-    response.setContentType("text/html; charset=UTF-8");
+    response.setContentType("application/json; charset=UTF-8");
     try {
       BeansUtils.getFileUploadUtil().uploadFile(request, response);
     } catch (Exception e) {
